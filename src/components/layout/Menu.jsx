b578c5760e58229827c0e05252c90828e0e0d196
leonardo <leonardosantos45548@gmail.com>
2022-02-09 15:48:27 -0300

    first commit

---
+++ src/components/layout/Menu.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Menu.css';
+
+
+const Menu = props => {
+  return (
+    <aside className="Menu">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Início</Link>
+              </li>
+              <li>
+                <Link to="/param/123">Param #01</Link>
+              </li>
+              <li>
+                <Link to="/param/pages-param">Param #02</Link>
+              </li>
+              <li>
+                <Link to="/about">Sobre</Link>
+              </li>
+              <li>
+                <Link to="/erro">Erro</Link>
+              </li>
+            </ul>
+          </nav>
+    </aside>
+  );
+}
+
+export default Menu;
